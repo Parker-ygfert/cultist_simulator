1fce06be6f0ca77be96ce0c0d8e69ccab35d774a
Parker <ygfert@gmail.com>
2022-09-16 04:24:56 +0800

    add i18n

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,23 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import { watch } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+const { t, locale } = useI18n()
+
+watch(locale, (newlocale) => {
+  localStorage.setItem('locale', newlocale)
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <nav>
+    <select v-model="locale">
+      <option>zh-TW</option>
+      <option>en</option>
+    </select>
+  </nav>
+  <h1>{{ $t('msg') }}</h1>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
+import { createI18n } from 'vue-i18n'
 import './style.css'
 import App from './App.vue'
+import en from './locales/en.json'
+import tw from './locales/zh-TW.json'
 
-createApp(App).mount('#app')
+const i18n = createI18n({
+  legacy: false,
+  locale: localStorage.getItem('locale') ?? 'en',
+  fallbackLocale: 'en',
+  messages: {
+    'en': en,
+    'zh-TW': tw
+  }
+})
+
+createApp(App).use(i18n).mount('#app')
